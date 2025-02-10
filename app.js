09aa05417eea9ff85033a7d6c125da073d83b52f
Sanjana Raghuwanshi <raghuneha2803@gmail.com>
2025-02-10 12:09:18 +0530

    graphql changes

--- app.js
+++ app.js
@@ -5,23 +5,30 @@ const bodyParser = require('body-parser');
 const multer = require('multer');
 // const { v4: uuidv4 } = require('uuid');
 
-const feedRoutes = require('./routes/feed');
-const authRoutes = require('./routes/auth');
+// const feedRoutes = require('./routes/feed');
+// const authRoutes = require('./routes/auth');
+
+const graphqlHTTP = require('express-graphql').graphqlHTTP;
+
+const graphqlSchema = require('./graphql/schema');
+const graphqlResolver = require('./graphql/resolvers');
+const schema = require('./graphql/schema');
+const auth = require('./middleware/auth'); //rename for graphql from is-auth to auth
 
 const app = express();
 
 const fileStorage = multer.diskStorage({
-    destination: (req, file, cb) =>{
+    destination: (req, file, cb) => {
         cb(null, 'images');
     },
-    filename: (req, file, cb) =>{
+    filename: (req, file, cb) => {
         // cb(null, uuidv4())
         cb(null, file.originalname);
     }
 });
 
 const fileFilter = (req, file, cb) => {
-    if(
+    if (
         file.mimetype === 'image/png' ||
         file.mimetype === 'image/jpg' ||
         file.mimetype === 'image/jpeg'
@@ -36,30 +43,53 @@ const fileFilter = (req, file, cb) => {
 // app.use(bodyParser.urlencoded()); //x-www-form-urlencoded <form>
 
 app.use(bodyParser.json()); // application/json
-app.use(multer({storage: fileStorage, fileFilter: fileFilter}).single('image'));
+app.use(multer({ storage: fileStorage, fileFilter: fileFilter }).single('image'));
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'OPTIONS,GET,POST,PUT,PATCH,DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
     next();
 });
 
-app.use('/feed', feedRoutes);
-app.use('/auth', authRoutes);
+// app.use('/feed', feedRoutes);
+// app.use('/auth', authRoutes);
+
+app.use(auth);
+
+app.use(
+    '/graphql',
+    graphqlHTTP({
+        schema: graphqlSchema,
+        rootValue: graphqlResolver,
+        graphiql: true,
+        formatError(err) {
+            if (!err.originalError) {
+                return err;
+            }
+            const data = err.originalError.data;
+            const message = err.message || 'An error occurred.';
+            const code = err.originalError.code || 500;
+            return { message: message, status: code, data: data };
+        }
+    })
+);
 
 app.use((error, req, res, next) => {
     console.log(error);
     const status = error.statusCode || 500;
     const message = error.message;
     const data = error.data;
-    res.status(status).json({message: message, data: data});
+    res.status(status).json({ message: message, data: data });
 })
 
 mongoose.connect('mongodb+srv://raghuneha2803:Neha1111@cluster0.zlsqh.mongodb.net/messages?retryWrites=true&w=majority&appName=Cluster0')
-    .then(result=>{
+    .then(result => {
         app.listen(8080);
-    }).catch(err=>{
-    console.log(err);
-})
+    }).catch(err => {
+        console.log(err);
+    })
--- frontendApp/src/App.js
+++ frontendApp/src/App.js
@@ -58,37 +58,67 @@ class App extends Component {
 
   loginHandler = (event, authData) => {
     event.preventDefault();
+    const graphqlQuery = {
+      query: `
+        {
+          login(email: "${authData.email}", password: "${authData.password}") {
+            token
+            userId
+          }
+        }
+      `
+    };
+
+    //rest-api code
+
+    // fetch('http://localhost:8080/auth/login', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json'
+    //   },
+    //   body: JSON.stringify({
+    //     email: authData.email,
+    //     password: authData.password
+    //   })
+    // })
     this.setState({ authLoading: true });
-    fetch('http://localhost:8080/auth/login', {
+    fetch('http://localhost:8080/graphql', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({
-        email: authData.email,
-        password: authData.password
-      })
+      body: JSON.stringify(graphqlQuery)
     })
       .then(res => {
-        if (res.status === 422) {
-          throw new Error('Validation failed.');
-        }
-        if (res.status !== 200 && res.status !== 201) {
-          console.log('Error!');
-          throw new Error('Could not authenticate you!');
-        }
         return res.json();
+        //rest-api code
+
+        // if (res.status === 422) {
+        //   throw new Error('Validation failed.');
+        // }
+        // if (res.status !== 200 && res.status !== 201) {
+        //   console.log('Error!');
+        //   throw new Error('Could not authenticate you!');
+        // }
       })
       .then(resData => {
+        if (resData.errors && resData.errors[0].status === 422) {
+          throw new Error(
+            "Validation failed. Make sure the email address isn't used yet!"
+          );
+        }
+        if (resData.errors) {
+          throw new Error('User login failed!');
+        }
         console.log(resData);
         this.setState({
           isAuth: true,
-          token: resData.token,
+          token: resData.data.login.token,
           authLoading: false,
-          userId: resData.userId
+          userId: resData.data.login.userId
         });
-        localStorage.setItem('token', resData.token);
-        localStorage.setItem('userId', resData.userId);
+        localStorage.setItem('token', resData.data.login.token);
+        localStorage.setItem('userId', resData.data.login.userId);
         const remainingMilliseconds = 60 * 60 * 1000;
         const expiryDate = new Date(
           new Date().getTime() + remainingMilliseconds
@@ -109,33 +139,83 @@ class App extends Component {
   signupHandler = (event, authData) => {
     event.preventDefault();
     this.setState({ authLoading: true });
-    fetch('http://localhost:8080/auth/signup', {
-      method: 'PUT',
+
+    //rest-api code
+
+    // fetch('http://localhost:8080/auth/signup', {
+    //   method: 'PUT',
+    //   headers: {
+    //     'Content-Type': 'application/json'
+    //   },
+    //   body: JSON.stringify({
+    //     email: authData.signupForm.email.value,
+    //     password: authData.signupForm.password.value,
+    //     name: authData.signupForm.name.value
+    //   })
+    // })
+
+
+    const graphqlQuery = {
+      query: `
+        mutation {
+          createUser(userInput: {email: "${authData.signupForm.email.value
+        }", name:"${authData.signupForm.name.value}", password:"${authData.signupForm.password.value
+        }"}) {
+            _id
+            email
+          }
+        }
+      `
+    };
+    fetch('http://localhost:8080/graphql', {
+      method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({
-        email: authData.signupForm.email.value,
-        password: authData.signupForm.password.value,
-        name: authData.signupForm.name.value
-      })
+      body: JSON.stringify(graphqlQuery)
     })
       .then(res => {
-        if (res.status === 422) {
+
+        //rest-api code
+
+        // if (res.status === 422) {
+        //   throw new Error(
+        //     "Validation failed. Make sure the email address isn't used yet!"
+        //   );
+        // }
+        // if (res.status !== 200 && res.status !== 201) {
+        //   console.log('Error!');
+        //   throw new Error('Creating a user failed!');
+        // }
+        return res.json();
+      })
+      .then(resData => {
+
+        //graphql code added
+
+        if (resData.errors && resData.errors[0].status === 422) {
           throw new Error(
             "Validation failed. Make sure the email address isn't used yet!"
           );
         }
-        if (res.status !== 200 && res.status !== 201) {
-          console.log('Error!');
-          throw new Error('Creating a user failed!');
+        if (resData.errors) {
+          throw new Error('User creation failed!');
         }
-        return res.json();
-      })
-      .then(resData => {
         console.log(resData);
-        this.setState({ isAuth: false, authLoading: false });
-        this.props.history.replace('/');
+        this.setState({
+          isAuth: true,
+          token: resData.data.login.token,
+          authLoading: false,
+          userId: resData.data.login.userId
+        });
+        localStorage.setItem('token', resData.data.login.token);
+        localStorage.setItem('userId', resData.data.login.userId);
+        const remainingMilliseconds = 60 * 60 * 1000;
+        const expiryDate = new Date(
+          new Date().getTime() + remainingMilliseconds
+        );
+        localStorage.setItem('expiryDate', expiryDate.toISOString());
+        this.setAutoLogout(remainingMilliseconds);
       })
       .catch(err => {
         console.log(err);
